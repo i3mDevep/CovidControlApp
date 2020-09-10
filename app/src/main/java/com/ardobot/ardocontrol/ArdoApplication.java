a92a15443eadedc98a1d7889a8f2e76327a807b9
DESKTOP-LB6ON16\Michael <mayxool.11@gmail.com>
2020-09-10 10:18:10 -0500

    update licensie sdk

--- app/src/main/java/com/ardobot/ardocontrol/ArdoApplication.java
+++ app/src/main/java/com/ardobot/ardocontrol/ArdoApplication.java
@@ -15,7 +15,8 @@ public class ArdoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        MicroblinkSDK.setLicenseKey("sRwAAAAXY29tLmFyZG9ib3QuYXJkb2NvbnRyb2zPRUkxXzSgn81amnHWnrBiCkAZZmKF+yK4Xn+v40Qd0fBEvkLgwk6iCOXl1+KDFVH6KPZrWo4+0WkEjn+CZV2AI4w7+ntYbjfxrJCCS5nYYJCUqZA9bZ+aiSMXxRrOnuK54Zcy", this);
+        MicroblinkSDK.setLicenseKey("sRwAAAAXY29tLmFyZG9ib3QuYXJkb2NvbnRyb2zPRUkxXzSgn81amnHWfrJiZqvltR285QRve1JVm0Av46JQjlh0N4tQr98UhDFlnvdpg/ic4RuUZeYoVVeF0tvUJ6QBKZes6Hmf6K6AbVFzwpblzf6pRQ8FA6MuLbqxN/ByUHaa", this);
+        //MicroblinkSDK.setLicenseKey("sRwAAAAXY29tLmFyZG9ib3QuYXJkb2NvbnRyb2zPRUkxXzSgn81amnHWnrBiCkAZZmKF+yK4Xn+v40Qd0fBEvkLgwk6iCOXl1+KDFVH6KPZrWo4+0WkEjn+CZV2AI4w7+ntYbjfxrJCCS5nYYJCUqZA9bZ+aiSMXxRrOnuK54Zcy", this);
         MicroblinkSDK.setIntentDataTransferMode(IntentDataTransferMode.PERSISTED_OPTIMISED);
 
     }
